06056cf1e8ccbe9a7efe1b3f5dbecdfa6e28df72
Yenna <kraysung.001@gmail.com>
2026-01-25 19:18:07 -0300

    ver 0.0007

--- ---Test--FarmGame---/Assets/Scripts/Player/PlayerMovement.cs
+++ ---Test--FarmGame---/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,21 +6,21 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Configuración de Movimiento")]
     public float speed = 5f;
-    public float verticalCompensation = 1.25f;
-    
-    
+    public float verticalCompensation = 1.25f; 
+
     private Rigidbody rb;
     private Vector3 moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     void Update()
     {
-        
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
@@ -29,11 +29,14 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        // 1. Calculamos la velocidad deseada en X y Z
         Vector3 targetVelocity = moveInput * speed;
-
         targetVelocity.z *= verticalCompensation;
 
-        targetVelocity.y = rb.velocity.y;
+        // En lugar de "rb.velocity.y" (que conserva saltos y rebotes),
+        // ponemos un valor negativo fijo (ej. -2 o -5).
+        // Esto actúa como un imán que lo mantiene pegado al suelo constantemente.
+        targetVelocity.y = -5f;
 
         rb.velocity = targetVelocity;
     }
