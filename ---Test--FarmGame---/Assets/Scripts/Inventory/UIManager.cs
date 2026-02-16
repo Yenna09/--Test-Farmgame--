161b0c77b9d39f0963adf38a413a8dc3792571d4
Yenna <kraysung.001@gmail.com>
2026-02-16 20:37:41 -0300

    act inventory

--- ---Test--FarmGame---/Assets/Scripts/Inventory/UIManager.cs
+++ ---Test--FarmGame---/Assets/Scripts/Inventory/UIManager.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager Instance;
+
+    public Transform hotbarContainer;   // Asigna el HotbarPanel
+    public Transform backpackContainer; // Asigna el BackpackPanel
+
+    public GameObject slotPrefab;
+
+    private void Start()
     {
-        
+        Instance = this;
+        InitializeSlots();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitializeSlots()
     {
-        
+        // Crear 9 slots visuales en la Hotbar
+        for (int i = 0; i < 9; i++)
+        {
+            Instantiate(slotPrefab, hotbarContainer);
+        }
+        // Crear 18 slots visuales en la Mochila
+        for (int i = 0; i < 18; i++)
+        {
+            Instantiate(slotPrefab, backpackContainer);
+        }
+    }
+
+    public void UpdateUI()
+    {
+        var slotsData = InventoryManager.Instance.inventorySlots;
+
+        // Actualizar Hotbar (Indices 0-8)
+        for (int i = 0; i < 9; i++)
+        {
+            UpdateSlotVisual(hotbarContainer.GetChild(i), slotsData[i]);
+        }
+
+        // Actualizar Mochila (Indices 9-26)
+        // OJO: El índice de la mochila visual empieza en 0, pero en la data empieza en 9
+        for (int i = 0; i < 18; i++)
+        {
+            UpdateSlotVisual(backpackContainer.GetChild(i), slotsData[i + 9]);
+        }
+    }
+
+    void UpdateSlotVisual(Transform slotObj, InventorySlot data)
+    {
+        Image icon = slotObj.Find("Icon").GetComponent<Image>();
+        // Lógica para activar/desactivar icono según si data.IsEmpty
     }
 }
